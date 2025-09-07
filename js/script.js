2d828ff85565c84a38dec4589b0e12a1e426520c
Atef Hejazi <78237128+atefhejazi1@users.noreply.github.com>
2025-09-07 14:21:57 +0300

    make aside suitable for mobile -responsive

--- js/script.js
+++ js/script.js
@@ -7,3 +7,26 @@ let topRightNavbar = document.querySelector(
 btnToggler.onclick = function () {
   topRightNavbar.classList.toggle("makeItFlex");
 };
+
+let togglerBtns = document.querySelectorAll("[data-toggle]");
+let dataBs = document.querySelectorAll("[data-bs-toggle]");
+
+togglerBtns.forEach((btnPlus) => {
+  btnPlus.onclick = function () {
+    dataBs.forEach((data) => {
+      if (btnPlus.dataset.toggle == data.dataset.bsToggle) {
+        // data.style.display = "none";
+        data.classList.toggle("appearData");
+      }
+    });
+  };
+});
+
+const btn = document.querySelector("main #filter");
+const sidebar = document.querySelector("main aside");
+
+btn.addEventListener("click", () => {
+  console.log("hello");
+
+  sidebar.classList.toggle("active");
+});
